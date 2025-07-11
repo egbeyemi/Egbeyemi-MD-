d0b288d5fda673ca0f77768615fe653f83f252d5
Mr-Anonymous <abdulrahmonegbeyemi@gmail.com>
2025-07-11 13:49:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  SESSION_ID: process.env.SESSION_ID,
+  OWNER_NUMBER: process.env.OWNER_NUMBER,
+  OWNER_NAME: process.env.OWNER_NAME,
+  BOT_NAME: process.env.BOT_NAME,
+  PACK_NAME: process.env.PACK_NAME,
+  AUTHOR_NAME: process.env.AUTHOR_NAME,
+};
